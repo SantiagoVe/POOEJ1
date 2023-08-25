4fc81ad8dec1d0facbfbbbd2afd8a12b6275d0c1
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2023-08-25 18:08:24 -0500

    POO Ejercicio Propuestos 12, 14 y 17 y POO Ejercicios realizados 4 y 5.

---
+++ src/main/java/com/mycompany/pooepn17/POOER4.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pooepn17;
+
+/**
+ *
+ * @author SantiaG0D
+ */
+public class POOER4 {
+    public static void main(String[] args) {
+        int edmama, edalber, edana, edjuan;
+        edjuan = 9;//edad de Juan
+        edalber = 2*(edjuan/3); //edad alberto
+        edana = 4 *(edjuan/3); //edad de Ana
+        edmama = edjuan+edalber+edana;
+        System.out.println("Juan = " + edjuan);
+        System.out.println("Alberto = " + edalber);
+        System.out.println("Ana = " + edana);
+        System.out.println("Mama = "+ edmama);
+    }
+}
---
+++ src/main/java/com/mycompany/pooepn17/POOER5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pooepn17;
+
+/**
+ *
+ * @author SantiaG0D
+ */
+public class POOER5 {
+    public static void main(String[] args) {
+        double suma = 0;
+        double x = 20;
+        suma = suma + x;
+        double y = 40;
+        x = x +Math.pow(y, 2);
+        suma = suma+(x/y);
+        System.out.println("El valor de la suma es = "+ suma);
+    }
+}
